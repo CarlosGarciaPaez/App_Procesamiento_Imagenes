3d59447b1c761be3e9124b488677e02c595713cf
CarlosGarciaPaez <cgarciapaez15@gmail.com>
2023-06-15 14:59:07 -0600

    Detección de objetos rojos, verdes y azules

--- app/src/main/java/com/example/procesaimagenes/MainActivity.java
+++ app/src/main/java/com/example/procesaimagenes/MainActivity.java
@@ -1,23 +1,29 @@
 package com.example.procesaimagenes;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.media.ExifInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
 import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
@@ -28,26 +34,28 @@ import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
     Button select,camara,btnGray;
     ImageView imageView;
     Bitmap bitmap;
     Mat mat, matGray;
     int SELECT_CODE = 100, CAMERA_CODE=101;
-
+    private Button colorButton;
+    private int selectedColor = 0;
+    private Button highlightButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
 
         if (OpenCVLoader.initDebug()) Log.d("LOADED", "success");
-        else Log.d("LOADED", "error");
+        else Log.d("LOADED", "err");
 
         getPermission();
 
@@ -88,8 +96,6 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode==SELECT_CODE && data!=null){
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),data.getData());
-
-                // Mostrar el bitmap rotado en el ImageView
                 imageView.setImageBitmap(bitmap);
 
                 mat = new Mat(); //Mat es una estructura de datos utilizada para representar matrices multidimensionales
@@ -97,6 +103,10 @@ public class MainActivity extends AppCompatActivity {
                 // Convertir el bitmap a una matriz de OpenCV
                 Utils.bitmapToMat(bitmap,mat);
 
+                /*
+                Imgproc.cvtColor(mat,matGray,Imgproc.COLOR_RGB2BGR);
+                Utils.matToBitmap(mat,bitmap);
+                imageGray.setImageBitmap(bitmap);*/
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -104,14 +114,16 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode==CAMERA_CODE && data!=null){
             bitmap = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(bitmap);
-            
+
             mat = new Mat();
             Utils.bitmapToMat(bitmap,mat);
         }
     }
     void getPermission(){
-        if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(new String[]{Manifest.permission.CAMERA},102);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.CAMERA},102);
+            }
         }
     }
 
@@ -126,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void convertirAGris(){
         // Convertir la imagen a escala de grises
-        matGray = new Mat();
+        Mat matGray = new Mat();
         Imgproc.cvtColor(mat, matGray, Imgproc.COLOR_BGR2GRAY);
 
         // Convertir la matriz de OpenCV a un bitmap
@@ -159,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
         Core.extractChannel(image, redChannel, 0);
         // Convertir los canales a formato RGB para mostrarlos en los ImageViews
         Mat rgbRedChannel = new Mat();
-        Imgproc.cvtColor(redChannel, rgbRedChannel, Imgproc.COLOR_BGR2RGB);
+        Imgproc.cvtColor(redChannel, rgbRedChannel, Imgproc.COLOR_GRAY2RGB);
         Bitmap redBitmap = Bitmap.createBitmap(rgbRedChannel.cols(), rgbRedChannel.rows(), Bitmap.Config.RGB_565);
         Utils.matToBitmap(rgbRedChannel, redBitmap);
         imageView.setImageBitmap(redBitmap);
@@ -173,7 +185,7 @@ public class MainActivity extends AppCompatActivity {
         Core.extractChannel(image, greenChannel, 1);
         // Convertir los canales a formato RGB para mostrarlos en los ImageViews
         Mat rgbGreenChannel = new Mat();
-        Imgproc.cvtColor(greenChannel, rgbGreenChannel, Imgproc.COLOR_BGR2RGB);
+        Imgproc.cvtColor(greenChannel, rgbGreenChannel, Imgproc.COLOR_GRAY2RGB);
         Bitmap greenBitmap = Bitmap.createBitmap(rgbGreenChannel.cols(), rgbGreenChannel.rows(), Bitmap.Config.RGB_565);
         Utils.matToBitmap(rgbGreenChannel, greenBitmap);
         imageView.setImageBitmap(greenBitmap);
@@ -188,39 +200,42 @@ public class MainActivity extends AppCompatActivity {
         Core.extractChannel(image, blueChannel, 2);
         // Convertir los canales a formato RGB para mostrarlos en los ImageViews
         Mat rgbBlueChannel = new Mat();
-        Imgproc.cvtColor(blueChannel, rgbBlueChannel, Imgproc.COLOR_BGR2RGB);
+        Imgproc.cvtColor(blueChannel, rgbBlueChannel, Imgproc.COLOR_GRAY2RGB);
         Bitmap blueBitmap = Bitmap.createBitmap(rgbBlueChannel.cols(), rgbBlueChannel.rows(), Bitmap.Config.RGB_565);
         Utils.matToBitmap(rgbBlueChannel, blueBitmap);
         imageView.setImageBitmap(blueBitmap);
     }
     public void detectarContornos(View view){
 
-        // Convertir el bitmap a una matriz de OpenCV
+// Convertir el bitmap a una matriz de OpenCV
         Mat image = new Mat();
         Utils.bitmapToMat(bitmap, image);
 
-        // Convertir la imagen a escala de grises
+// Convertir la imagen a escala de grises
         Mat grayImage = new Mat();
         Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
 
-        // Aplicar un umbral para obtener una imagen binaria
+// Aplicar un umbral para obtener una imagen binaria
         Mat binaryImage = new Mat();
         Imgproc.threshold(grayImage, binaryImage, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
 
-        // Encontrar los contornos en la imagen binaria
+// Encontrar los contornos en la imagen binaria
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(binaryImage, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        // Dibujar los contornos en la imagen original
+// Dibujar los contornos en la imagen original
         Mat resultImage = image.clone();
         Imgproc.drawContours(resultImage, contours, -1, new Scalar(0, 255, 0), 2);
 
-        // Mostrar la imagen con los contornos
+// Mostrar la imagen con los contornos
         Bitmap resultBitmap = Bitmap.createBitmap(resultImage.cols(), resultImage.rows(), Bitmap.Config.RGB_565);
         Utils.matToBitmap(resultImage, resultBitmap);
         imageView.setImageBitmap(resultBitmap);
+
     }
+
+
     public void rotarImagen(View view) {
 
         // Especifica el ángulo de rotación deseado (en grados)
@@ -233,5 +248,85 @@ public class MainActivity extends AppCompatActivity {
         // Muestra el Bitmap rotado en el ImageView
         imageView.setImageBitmap(bitmap);
     }
+    public void pantalla_2 (View view){
+        //setContentView(R.layout.activity_main2);
+
+    }
+
+    public void objetosRed(View view) {
+
+        Mat img=new Mat();
+        Utils.bitmapToMat(bitmap, img);
+
+        Mat hsv_img = new Mat();
+        Imgproc.cvtColor(img, hsv_img, Imgproc.COLOR_BGR2HSV);
+
+        Scalar min_rojo = new Scalar(0, 100, 100);
+        Scalar max_rojo = new Scalar(10, 255, 255);
+
+        Mat mascara_roja = new Mat();
+        Core.inRange(hsv_img, min_rojo, max_rojo, mascara_roja);
+
+        Mat objetos_rojos = new Mat();
+        Core.bitwise_and(img, img, objetos_rojos, mascara_roja);
+
+        // Ahora puedes hacer lo que desees con los resultados (objetos_rojos, objetos_verdes, objetos_azules)
+        // Por ejemplo, convertirlos a Bitmap y ostracism en un ImageView:
+        Bitmap resultBitmapRojo = Bitmap.createBitmap(objetos_rojos.cols(), objetos_rojos.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(objetos_rojos, resultBitmapRojo);
+        imageView.setImageBitmap(resultBitmapRojo);
+    }
+
+    public void objetosGreen(View view) {
+        Mat img=new Mat();
+        Utils.bitmapToMat(bitmap, img);
+        Mat rgb_img = new Mat();
+        Imgproc.cvtColor(img, rgb_img, Imgproc.COLOR_BGR2RGB);
+
+        Mat hsv_img = new Mat();
+        Imgproc.cvtColor(img, hsv_img, Imgproc.COLOR_BGR2HSV);
+
+        Scalar min_verde = new Scalar(40, 100, 100);
+        Scalar max_verde = new Scalar(80, 255, 255);
+
+        Mat mascara_verde = new Mat();
+        Core.inRange(hsv_img, min_verde, max_verde, mascara_verde);
+
+        Mat objetos_verdes = new Mat();
+        Core.bitwise_and(rgb_img, rgb_img, objetos_verdes, mascara_verde);
+
+        // Ahora puedes hacer lo que desees con los resultados (objetos_rojos, objetos_verdes, objetos_azules)
+        // Por ejemplo, convertirlos a Bitmap y mostrarlos en un ImageView:
+        Bitmap resultBitmapVerde = Bitmap.createBitmap(objetos_verdes.cols(), objetos_verdes.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(objetos_verdes, resultBitmapVerde);
+        imageView.setImageBitmap(resultBitmapVerde);
+    }
+    public void objetosBlue(View view) {
+
+        Mat img=new Mat();
+        Utils.bitmapToMat(bitmap, img);
+
+        Mat hsv_img = new Mat();
+        Imgproc.cvtColor(img, hsv_img, Imgproc.COLOR_BGR2HSV);
+
+        Scalar min_azul = new Scalar(100, 100, 100);
+        Scalar max_azul = new Scalar(130, 255, 255);
+
+
+        Mat mascara_azul = new Mat();
+        Core.inRange(hsv_img, min_azul, max_azul, mascara_azul);
+
+        Mat objetos_azules = new Mat();
+        Core.bitwise_and(img, img, objetos_azules, mascara_azul);
+
+        // Ahora puedes hacer lo que desees con los resultados (objetos_rojos, objetos_verdes, objetos_azules)
+        // Por ejemplo, convertirlos a Bitmap y mostrarlos en un ImageView:
+        Bitmap resultBitmapAzul = Bitmap.createBitmap(objetos_azules.cols(), objetos_azules.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(objetos_azules, resultBitmapAzul);
+        imageView.setImageBitmap(resultBitmapAzul);
+    }
+
+
+
 
 }
